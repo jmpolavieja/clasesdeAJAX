a92a39ce46e53b47c002ba4c000650c87f2807ac
Josemaria Garcia de Polavieja Ferre <jgf@altair.edu.es>
2020-04-20 09:47:18 +0200

    premaster con bootstrap y botón

--- funciones.js
+++ funciones.js
@@ -1,13 +1,23 @@
-window.onload = function() {
+
+function cargaDatos() {
+// Crear un objeto del tipo XMLHttpRequest
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://reqres.in/api/users", true);
+    // El método open, establece los parámetros de la petición al servidor
+    xhr.open("GET", "https://reqres.in/api/users/2", true);
     console.log("Estado de la petición " + xhr.status);
+    // Evento que se dispara cuando se cargan los datos
     xhr.onload = function(){
         console.log("Nuevo estado de la petición: " + xhr.status);
         console.log(xhr.responseText);
         var datos = JSON.parse(xhr.responseText);
         console.log(datos.data);
+        var subtitulo = document.getElementById('subtitulo');
+        var nombre = document.getElementById('nombre');
+        var avatar = document.getElementById('avatar');
+        subtitulo.innerText += " " + datos.data.id;
+        nombre.innerText += datos.data.first_name + " " + datos.data.last_name;
+        avatar.src = datos.data.avatar;
     };
+    // Método para enviar la petición al servidor
     xhr.send();
 }
-
